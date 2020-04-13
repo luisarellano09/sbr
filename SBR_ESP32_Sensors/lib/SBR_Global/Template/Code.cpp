4a6d463f55fc1209fbb5a14f7f2f88a4c29e2d28
Luis Arellano <luis.arellano09@gmail.com>
2020-04-13 14:15:57 +0200

    Lib management

---
+++ SBR_ESP32_Sensors/lib/SBR_Global/Template/Code.cpp
@@ -0,0 +1,24 @@
+/**
+ * \file doxygen_template.h
+ * \author The Author
+ * \date 20 Jun 2016
+ * \todo TODO for this file.
+ *
+ * \brief A brief description of the file
+ *
+ * A more detailed description can be placed here.
+ * Multiple lines of description have to start with *.
+ *
+ * If you want to end a command, add an empty line (starting with *).
+ *
+ * Inside of a comment block, the user can use doxygen commands like "brief" etc..
+ * A list of all available commands can be found at \see https://www.stack.nl/~dimitri/doxygen/manual/commands.html .
+ *
+ * You can reference to entries in the documentation by using #. Example: #myEnum_t.
+ * 
+ * 
+ * Changes
+ * 13.04.2020: Doc was created
+ * 
+ *
+ */
---
+++ SBR_ESP32_Util/lib/SBR_Global/Definition/GlobalDef.h
@@ -0,0 +1,12 @@
+#ifndef ENUMTEST_H
+#define ENUMTEST_H
+
+
+enum REQ { READ, WRITE };
+
+
+
+
+
+
+#endif // ENUMTEST_H
