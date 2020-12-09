a6f370996c12224cd565dbe3049ed252330a9295
jorgesalgado33 <jorgesalgado23@gmail.com>
2020-12-09 10:48:58 +0100

    Updated file PID

--- SBR_ESP32/SBR_ESP32_Sensors/src/PID/PID_Manager.h
+++ SBR_ESP32/SBR_ESP32_Sensors/src/PID/PID_Manager.h
@@ -16,12 +16,16 @@
  *******************************************************************************************************************************************/
 #include "../Definition/GlobalDef.h"
 
-#define P_Gain  11.0
-#define I_Gain  1.0
-#define D_Gain  10.0
+#define P_Gain  (float)120.0f
+#define I_Gain  (float)20.0f
+#define D_Gain  (float)60.0f
 
-#define PositionDesired     (float)0.0
+#define POSITIONDESIRED     (float)0.0f
+#define MAX_RADIAN          (float)0.87f          //miliRadian = 50°
+#define MIN_RADIAN          (float)-0.87f         //miliRadian = 50°
 
+#define MAX_ACC_INTEGRAL    (float)4.00f
+#define MIN_ACC_INTEGRAL    (float)-4.00f
 /*******************************************************************************************************************************************
  *  												IMUManager Class
  *******************************************************************************************************************************************/
@@ -34,8 +38,11 @@ class PID_Manager{
 
 public: 
     /*Global Variables*/
-    float Prev_error = 0.0;
-    float IState =0.0;
+    float PIDSetpoint = POSITIONDESIRED;       
+    float Kp    = P_Gain;   // Kp -  proportional gain
+    float Ki    = I_Gain;   // Ki -  Integral gain
+    float Kd    = D_Gain;   // Kd -  derivative gain
+
     /**
      * @brief Construct a new Motor Manager object
      * 
@@ -44,7 +51,7 @@ public:
      * @param 
      */
     PID_Manager();
-
+    PID_Manager(float _Setpoint, float _kp, float _ki, float _kd);
     /**
      * @brief Destroy the Motor Manager object
      * 
@@ -57,12 +64,31 @@ public:
      * @return RC_e Result code
      */
 
-    float UpdatePID(float PositionMesured);
+    RC_e UpdatePID(float _PositionMesured, float *_PiD_Result);
+
+    
+    RC_e GetConsigne(float *_Setpoint);
+
+    RC_e SetConsigne(float _Setpoint);
+
+
+    RC_e GetKp(float *_Kp);
+    RC_e GetKi(float *_Ki);
+    RC_e GetKd(float *_Kd);
+
+    RC_e SetKp(float _kp);
+    RC_e SetKi(float _ki);
+    RC_e SetKd(float _kd);
 
 private:
+    /*Variable*/
+    float Prev_error = 0.0f;     //  e(k-1)
+    float AccIntegral = 0.0f;
+    /*functions*/
     float UpdateP(float error);  
     float UpdateI(float error);  
     float UpdateD(float error, float Prev_error);  
+    float ComputeError(float _PositionMesured);
 };
 
 #endif // PID_MANAGER_H
