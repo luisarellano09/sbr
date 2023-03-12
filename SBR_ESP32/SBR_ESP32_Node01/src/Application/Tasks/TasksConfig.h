c9689897dbbf15cf8e3ca71e9d04152ad4051120
Luis Arellano <luis.arellano09@gmail.com>
2023-03-12 05:38:11 +0100

    Refactor of config files

--- SBR_ESP32/SBR_ESP32_Node01/src/Application/Tasks/TasksConfig.h
+++ SBR_ESP32/SBR_ESP32_Node01/src/Application/Tasks/TasksConfig.h
@@ -1,7 +1,7 @@
 /**
  * @file TasksConfig.h
  * @author Luis Arellano (luis.arellano09@gmail.com)
- * @brief Tasks
+ * @brief Declarations of the Tasks class
  * @version 1.0
  * @date 03.02.2023
  * 
@@ -22,16 +22,64 @@
  *  												DECLARATION
  *******************************************************************************************************************************************/
 
+/**
+ * @brief InitTasks 
+ * 
+ */
 void InitTasks();
-void InitQueues();
-void TaskMonitoring();
-void TaskInfoPrint(TaskHandle_t* task);
 
+/**
+ * @brief Initialization of Queues 
+ * 
+ */
+void InitQueues();
+
+/**
+ * @brief MonitorTasks
+ * 
+ */
+void MonitorTasks();
+
+/**
+ * @brief Print Task information
+ * 
+ */
+void PrintTaskInfo(TaskHandle_t* task);
+
+/**
+ * @brief TaskCLI 
+ * 
+ */
 void TaskCLI(void *parameter);
+
+/**
+ * @brief TaskGetValueCLI 
+ * 
+ */
 void TaskGetValueCLI(void *parameter);
+
+/**
+ * @brief TaskOTA 
+ * 
+ */
 void TaskOTA(void *parameter);
+
+/**
+ * @brief TaskNodeESP32 
+ * 
+ */
 void TaskNodeESP32(void *parameter);
-void TaskReg10(void *parameter);
+
+/**
+ * @brief Task Modes
+ * 
+ */
 void TaskModes(void *parameter);
+
+/**
+ * @brief TaskReg10 
+ * 
+ */
+void TaskReg10(void *parameter);
 
 #endif // TASKSCONFIG_H
