db9792714caa08b6a918b096487f1de34d1f52ea
Luis Arellano <luis.arellano09@gmail.com>
2023-04-23 08:38:11 +0200

    Handling of registers

--- SBR_ESP32/SBR_ESP32_Manager/src/Application/CommunicationBus/NodeEsp32/NodeHandler.h
+++ SBR_ESP32/SBR_ESP32_Manager/src/Application/CommunicationBus/NodeEsp32/NodeHandler.h
@@ -31,10 +31,9 @@ RC_e ExtHandler(Request* request){
         return RC_e::ERROR_NULL_POINTER;
     }
 
-    //request->Print();
-
     Log.traceln("[NodeHandler::ExtHandler] Request received: nodeId=%d, reqType=%d, regId=%d, data=%d, CRC=%d", request->nodeId, request->reqType, request->regId, request->data, request->CRC);
 
+    // Handle requests
     switch(request->regId){
 
         case COM_REQUEST_REG_ID_e::STATUS_HEARTBEAT_ESP32_COUNTER_R: {
@@ -42,6 +41,33 @@ RC_e ExtHandler(Request* request){
             break;
         }
         
+        case COM_REQUEST_REG_ID_e::STATUS_HEARTBEAT_LINUX_COUNTER_R: {
+            manager->m_nodeLinux->TokenCounter(request->data, COM_REQUEST_REG_ID_e::STATUS_HEARTBEAT_LINUX_COUNTER_R);
+            break;
+        }
+
+        case COM_REQUEST_REG_ID_e::MODE_MANAGER_RESTART_W: {
+            Serial.println("Restarting...");
+            ESP.restart();
+            break;
+        }
+
+        case COM_REQUEST_REG_ID_e::MODE_MANAGER_PROGRAM_W: {
+            Serial.println("Program Mode.....");
+            StartMode(Modes_e::Mode_Program);
+            Log.infoln("[NodeHandler::MODE_MANAGER_PROGRAM_W] Mode Program started");
+            break;
+        }
+
+        case COM_REQUEST_REG_ID_e::MODE_MANAGER_SYNC_DATA_RW: {
+            // ToDo
+            break;
+        }
+
+        case COM_REQUEST_REG_ID_e::MODE_LINUX_SYNC_DATA_RW: {
+            // Todo
+            break;
+        }
        
     }
 
