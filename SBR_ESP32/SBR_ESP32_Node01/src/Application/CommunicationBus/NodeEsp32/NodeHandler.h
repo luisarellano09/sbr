347fe6992232e08a704778dfc39f999e808d0536
Luis Arellano <luis.arellano_zea@siemens.com>
2023-04-25 23:48:18 +0200

    Add start stop of motion

--- SBR_ESP32/SBR_ESP32_Node01/src/Application/CommunicationBus/NodeEsp32/NodeHandler.h
+++ SBR_ESP32/SBR_ESP32_Node01/src/Application/CommunicationBus/NodeEsp32/NodeHandler.h
@@ -60,6 +60,7 @@ RC_e ExtHandler(Request* request){
         
         case COM_REQUEST_REG_ID_e::MODE_NODE1_START_W: {
             Serial.println("Motion Mode.....");
+            manager->m_motionControl->Start();
             StartMode(Modes_e::Mode_Motion);
             Log.infoln("[NodeHandler::MODE_NODE1_START_W] Mode Motion started");
             break;
@@ -67,6 +68,7 @@ RC_e ExtHandler(Request* request){
         
         case COM_REQUEST_REG_ID_e::MODE_NODE1_STOP_W: {
             Serial.println("Idle Mode.....");
+            manager->m_motionControl->Stop();
             StartMode(Modes_e::Mode_Idle);
             Log.infoln("[NodeHandler::MODE_NODE1_STOP_W] Mode Idle started");
             break;
