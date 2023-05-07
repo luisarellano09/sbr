1d0a6aee95b03317f2b87cee11382711b7663aa7
Luis Arellano <luis.arellano09@gmail.com>
2023-05-07 16:02:14 +0200

    Threads

--- SBR_SERV/serial_connector/src/main.rs
+++ SBR_SERV/serial_connector/src/main.rs
@@ -1,14 +1,17 @@
 
 mod node;
 use node::Node;
-
+use std::thread;
 
 fn main() {
     let mut node_linux: Node = Node::new(String::from("/dev/ttyS0"), 460800, 3000);
 
-    loop{
-        node_linux.run();
-    }
-    
+    let thread_node = thread::spawn(move || {
+        loop{
+            node_linux.run();
+        }
+    });
 
+    thread_node.join().unwrap();
+    
 }
