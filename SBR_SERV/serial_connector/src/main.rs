0b36a699785538137d2be47a4784a2a042d47912
Luis Arellano <luis.arellano09@gmail.com>
2023-05-07 15:46:24 +0200

    Change serial name

--- SBR_SERV/serial_connector/src/main.rs
+++ SBR_SERV/serial_connector/src/main.rs
@@ -4,7 +4,7 @@ use node::Node;
 
 
 fn main() {
-    let mut node_linux: Node = Node::new(String::from("/dev/ttyUSB1"), 460800, 3000);
+    let mut node_linux: Node = Node::new(String::from("/dev/ttyS0"), 460800, 3000);
 
     loop{
         node_linux.run();
