fdbb1f1bb2b518e2c113ce14f29b557601d23409
luisarellano09 <luis.arellano09@gmail.com>
2023-05-30 07:01:44 +0000

    mutations

--- SBR_SERV/graphql/src/graphql_mutations.rs
+++ SBR_SERV/graphql/src/graphql_mutations.rs
@@ -9,6 +9,53 @@ pub struct Mutations;
 #[graphql_object(Context = ContextGraphQL)]
 impl Mutations {
 
+    fn RestartEsp32Manager(_context: &ContextGraphQL) -> FieldResult<bool> {
+
+        publish_esp32_write("ESP32.WRITE.MODE.MANAGER.RESTART_W".to_string(), 1)?;
+            
+        Ok(true)
+    }
+
+
+    fn ProgramEsp32Manager(_context: &ContextGraphQL) -> FieldResult<bool> {
+
+        publish_esp32_write("ESP32.WRITE.MODE.MANAGER.PROGRAM_W".to_string(), 1)?;
+            
+        Ok(true)
+    }
+
+
+    fn SyncDataManager(_context: &ContextGraphQL, new_motor_left_speed: Option<f64>, new_motor_right_speed: Option<f64>) -> FieldResult<Esp32LiveMotors> {
+
+        match new_motor_left_speed {
+            Some(speed) => {
+                publish_esp32_write("ESP32.WRITE.LIVE.MOTOR_LEFT.SPEED_W".to_string(), (speed * 100.0) as i32)?;
+            }, 
+            None =>{ }
+        }
+
+        match new_motor_right_speed {
+            Some(speed) => {
+                publish_esp32_write("ESP32.WRITE.LIVE.MOTOR_RIGHT.SPEED_W".to_string(), (speed * 100.0) as i32)?;
+            }, 
+            None =>{ }
+        }
+
+        Ok(Esp32LiveMotors { 
+            motor_left_speed: new_motor_left_speed.unwrap_or_default(),
+            motor_right_speed: new_motor_right_speed.unwrap_or_default(),
+        })
+    }
+
+
+    fn RestartEsp32Node01(_context: &ContextGraphQL) -> FieldResult<bool> {
+
+        publish_esp32_write("ESP32.WRITE.MODE.MANAGER.RESTART_W".to_string(), 1)?;
+            
+        Ok(true)
+    }
+    
+
     fn SetMotorsSpeed(_context: &ContextGraphQL, new_motor_left_speed: Option<f64>, new_motor_right_speed: Option<f64>) -> FieldResult<Esp32LiveMotors> {
 
         match new_motor_left_speed {
@@ -30,5 +77,8 @@ impl Mutations {
             motor_right_speed: new_motor_right_speed.unwrap_or_default(),
         })
     }
+
+
+    
 
 }
