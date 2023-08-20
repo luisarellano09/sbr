8b114858136d842ff3867770375937ed55854272
Luis Arellano <luis.arellano09@gmail.com>
2023-08-21 00:02:27 +0200

    fix scroll down of sidebar

--- SBR_SERV/nextjs/src/components/NavLayout/Layout.jsx
+++ SBR_SERV/nextjs/src/components/NavLayout/Layout.jsx
@@ -6,21 +6,15 @@ import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
 const Layout = ({ children }) => {
-	const [collapsed, setSidebarCollapsed] = useState(false);
 	const [showSidebar, setShowSidebar] = useState(false);
 	return (
 		<div
 			className={classNames({
-				"grid min-h-screen bg-gradient-to-b from-[#1d2f46] to-[#132b25]": true,
-				"grid-cols-sidebar": !collapsed,
-				"grid-cols-sidebar-collapsed": collapsed,
-				"transition-[grid-template-columns] duration-300 ease-in-out": true,
+				"grid bg-gradient-to-b from-[#1d2f46] to-[#132b25]": true,
 				"grid md:grid-cols-[200px_auto]": true,
 			})}
 		>	
             <Sidebar
-                collapsed={collapsed}
-                setCollapsed={setSidebarCollapsed}
                 shown={showSidebar}
             />
 		
--- SBR_SERV/nextjs/src/components/NavLayout/Sidebar.jsx
+++ SBR_SERV/nextjs/src/components/NavLayout/Sidebar.jsx
@@ -1,44 +1,31 @@
 
 import classNames from "classnames";
-import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from "@heroicons/react/24/outline";
 import SidebarItems from "./SidebarItems";
 
-const Sidebar = ({
-	collapsed,
-	shown,
-	setCollapsed,
-}) => {
-	const Icon = collapsed ? ChevronDoubleRightIcon : ChevronDoubleLeftIcon;
+const Sidebar = ({shown}) => {
+
 	return (
 		<div
 			className={classNames({
-				"bg-gradient-to-b from-[#272b46] to-[#1e5c54] text-[#a0e4ab] fixed md:static md:translate-x-0 z-20": true,
-				"transition-all duration-300 ease-in-out ": true,
-				"w-[200px]": !collapsed,
-				"w-16": collapsed,
-				"-translate-x-full": !shown,
+				"bg-gradient-to-b from-[#272b46] to-[#1e5c54] text-[#a0e4ab]": true,
+				"transition-all duration-300 ease-in-out": true,
+                "fixed": true,
+                "md:w-full md:sticky md:z-10 md:shadow-sm md:h-[73px] md:top-0 md:translate-x-0 z-20": true,
+                "-translate-x-full": !shown,
 			})}
 		>
-			<div
-				className="flex flex-col justify-between h-screen md:h-full sticky inset-0">
-				{/* logo and collapse button */}
-				<div
-					className={classNames({
-						"flex items-center border-b border-b-slate-500 transition-none": true,
-						"p-4 justify-between": !collapsed,
-						"py-4 justify-center": collapsed,
-					})}
-				>
-					{!collapsed && (
-						<p className="font-mono font-bold text-4xl ml-10 text-[#39ac89]">SBR</p>
-					)}
+			<div className="flex flex-col justify-between h-screen sticky">
+	
+				<div className="flex items-center p-4 bg-gradient-to-r from-[#0e1730] to-[#272b46]">
+
+                    <p className="font-mono font-bold text-4xl ml-10 text-[#39ac89]">SBR</p>
+
 				</div>
 
-                <nav className="flex-grow">
+                <div className="flex-grow">
                     <SidebarItems />
-                </nav>
+                </div>
 				
-
 			</div>
 		</div>
 	);
