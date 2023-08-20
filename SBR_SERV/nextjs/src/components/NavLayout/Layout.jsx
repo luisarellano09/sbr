b3fec5f55de84a8bdd23358827bc884631981a97
Luis Arellano <luis.arellano09@gmail.com>
2023-08-21 00:07:03 +0200

    formating

--- SBR_SERV/nextjs/src/components/NavLayout/Layout.jsx
+++ SBR_SERV/nextjs/src/components/NavLayout/Layout.jsx
@@ -8,22 +8,19 @@ import Sidebar from "./Sidebar";
 const Layout = ({ children }) => {
 	const [showSidebar, setShowSidebar] = useState(false);
 	return (
-		<div
-			className={classNames({
-				"grid bg-gradient-to-b from-[#1d2f46] to-[#132b25]": true,
-				"grid md:grid-cols-[200px_auto]": true,
-			})}
-		>	
-            <Sidebar
-                shown={showSidebar}
-            />
+		<div className="grid bg-gradient-to-b from-[#1d2f46] to-[#132b25] md:grid-cols-[200px_auto]">	
+
+            <Sidebar shown={showSidebar} />
 		
-			<div>
-				<Navbar
+			<div> 
+				<Navbar 
 					onMenuButtonClick={() => setShowSidebar((prev) => !prev)}
 				/>
-				{children}
+                <div>
+                    {children}
+                </div>
 			</div>
+            
 		</div>
 	);
 };
