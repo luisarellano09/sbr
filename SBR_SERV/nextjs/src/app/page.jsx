1f712783b58c250ddd19ba8abbe244039b4adb61
Luis Arellano <luis.arellano09@gmail.com>
2023-08-22 20:54:23 +0200

    remove imports from home

--- SBR_SERV/nextjs/src/app/page.jsx
+++ SBR_SERV/nextjs/src/app/page.jsx
@@ -1,12 +1,4 @@
 
-import DashboardIMU from '@/components/Dashboards/DashboardIMU';
-import ChartIMU from '@/components/Charts/ChartIMU';
-import DashboardOdometry from '@/components/Dashboards/DashboardOdometry';
-import DashboardMotionPosition from '@/components/Dashboards/DashboardMotionPosition';
-import DashboardMotionAngle from '@/components/Dashboards/DashboardMotionAngle';
-import ChartMotionPosition from '@/components/Charts/ChartMotionPosition';
-import ChartMotionAngle from '@/components/Charts/ChartMotionAngle';
-
 export default function Home() {
 	return (
         <div className="h-[calc(100vh-74px)]">
