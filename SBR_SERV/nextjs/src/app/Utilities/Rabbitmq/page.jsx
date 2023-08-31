8841c5cae1cba483cc329b8771812eb376694e14
Luis Arellano <luis.arellano09@gmail.com>
2023-08-31 13:57:39 +0200

    links added

---
+++ SBR_SERV/nextjs/src/app/Utilities/Rabbitmq/page.jsx
@@ -0,0 +1,7 @@
+export default function Rabbitmq() {
+    return (
+        <div>
+            <iframe src="http://sbrpi/sbr_rabbitmq_management/" className="w-[100%] h-[100vh]"/>
+        </div>
+    );
+}
