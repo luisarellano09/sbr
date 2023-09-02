c502a9df4af84bd8b0c6e62cee2c000de06410df
Luis Arellano <luis.arellano09@gmail.com>
2023-09-02 12:35:56 +0200

    add host_connector

---
+++ SBR_SERV/host_connector/src/message_esp32.rs
@@ -0,0 +1,9 @@
+use serde::{Deserialize, Serialize};
+
+
+//=====================================================================================================
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct MessageEsp32 {
+    pub name: String,
+    pub data: i32,
+}
---
+++ SBR_SERV/host_connector/src/rabbitmq_consumer_esp32.rs
@@ -0,0 +1,87 @@
+#![allow(dead_code)]
+
+use std::error::Error;
+use amiquip::{Connection, ExchangeDeclareOptions, ExchangeType, QueueDeclareOptions, FieldTable, ConsumerOptions, ConsumerMessage};
+use crate::message_esp32::MessageEsp32;
+
+
+//=====================================================================================================
+pub struct RabbitmqConsumerESP32 {}
+
+
+//=====================================================================================================
+impl RabbitmqConsumerESP32 {
+
+    //=====================================================================================================
+    pub fn new() -> Self {
+        RabbitmqConsumerESP32 {
+            
+        }
+    }
+
+    
+    //=====================================================================================================
+    pub fn run(&mut self)  -> Result<(), Box<dyn Error>> {
+        // Open connection.
+        let mut connection = Connection::insecure_open("amqp://rabbitmq:La123456.@sbr_rabbitmq:5672/")?;
+
+        // Open a channel - None says let the library choose the channel ID.
+        let channel = connection.open_channel(None)?;
+
+        // Declare the exchange we will bind to.
+        let exchange = channel.exchange_declare(
+            ExchangeType::Topic,
+            "SBR_EXCH_READ_ESP32",
+            ExchangeDeclareOptions{
+                durable: false,
+                auto_delete: false,
+                internal: false,
+                ..Default::default()
+            },
+        )?;
+
+        // Declare the exclusive, server-named queue we will use to consume.
+        let queue = channel.queue_declare(
+            "Q_SBR_ESP32_TO_REDIS",
+            QueueDeclareOptions {
+                exclusive: true,
+                ..QueueDeclareOptions::default()
+            },
+        )?;
+
+        //Binding
+        queue.bind(&exchange, "ESP32.READ.#", FieldTable::new())?;
+
+        let consumer = queue.consume(ConsumerOptions {
+            no_ack: true,
+            ..ConsumerOptions::default()
+        })?;
+
+        println!("Rabbitmq config done");
+
+
+        // Loop wait for messages
+        println!("Listening for messages");
+        for (_, message) in consumer.receiver().iter().enumerate() {
+            match message {
+                ConsumerMessage::Delivery(delivery) => {
+                    let body = String::from_utf8_lossy(&delivery.body).to_string();
+                    match serde_json::from_str::<MessageEsp32>(&body){
+                        Ok(json) =>{
+                            //redis_connection.set(json.name, json.data)?;
+                        }, 
+                        Err(er) => {
+                            eprintln!("{}", er);
+                        }
+                    }
+                }
+                other => {
+                    println!("Consumer ended: {:?}", other);
+                    break;
+                }
+            }
+        }
+        
+        Ok(())
+    }
+}
