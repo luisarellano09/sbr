aa98881194d424aacec247c6e46bd311e45caa22
Luis Arellano <luis.arellano09@gmail.com>
2023-09-03 08:11:59 +0200

    add publish function to publich to host connector

--- SBR_SERV/graphql/src/rabbitmq_connection.rs
+++ SBR_SERV/graphql/src/rabbitmq_connection.rs
@@ -36,3 +36,34 @@ pub fn publish_esp32_write(key: String, data: i32) -> Result<(), Box<dyn Error>>
     Ok(())
 }
 
+
+//=====================================================================================================
+pub fn publish_host_connector(host: String, request: String) -> Result<(), Box<dyn Error>> {
+
+    // Open connection.
+    let mut connection = Connection::insecure_open(URL)?;
+
+    // Open a channel - None says let the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
+
+    // Declare the exchange we will bind to.
+    let exchange = channel.exchange_declare(
+        ExchangeType::Topic,
+        "SBR_EXCH_HOST_CONNECTOR",
+        ExchangeDeclareOptions{
+            durable: false,
+            auto_delete: false,
+            internal: false,
+            ..Default::default()
+        },
+    )?;
+
+    let mut key = String::from("HOST.");
+    key.push_str(host.as_str());
+    key.push_str(".");
+
+    
+    exchange.publish(Publish::new(request.as_bytes(), key))?;
+
+    Ok(())
+}
