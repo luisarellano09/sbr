08b445c2be9a54a7ea8d0850f08f1f4cca1700e5
Luis Arellano <luis.arellano09@gmail.com>
2023-09-17 07:42:39 +0200

    add title event

--- SBR_SERV/nextjs/src/app/page.jsx
+++ SBR_SERV/nextjs/src/app/page.jsx
@@ -1,7 +1,13 @@
+"use client"
+
+import { useStoreWeb } from "@/store/store";
 
 export default function Home() {
+
+    const SetCurrentPage = useStoreWeb((state) => state.SetCurrentPage);
+
 	return (
-        <div className="h-[calc(100vh-74px)]">
+        <div onLoad={SetCurrentPage("Home")} className="h-[calc(100vh-74px)]">
 
         </div>
     );
