cb7c66e5fabcc908e4465d68adaf9733a5930950
Luis Arellano <luis.arellano09@gmail.com>
2023-09-17 11:05:10 +0200

    title added

--- SBR_SERV/nextjs/src/components/NavLayout/Navbar.jsx
+++ SBR_SERV/nextjs/src/components/NavLayout/Navbar.jsx
@@ -1,14 +1,11 @@
 "use client"
 
-import { useStoreWeb } from "@/store/store";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import classNames from "classnames";
+import Titlebar from "./Titlebar";
 
 
 export default function Navbar(props) {
-
-    const hostName = useStoreWeb((state) => state.hostName);
-    const currentPage = useStoreWeb((state) => state.currentPage);
 
     return (
         <nav
@@ -18,12 +15,7 @@ export default function Navbar(props) {
             "w-screen md:w-full sticky z-10 px-4 shadow-sm h-[73px] top-0 ": true, //positioning & styling
             })}
         >
-            <div className="font-bold text-lg">
-                <p>{hostName}</p>
-                <p>{currentPage}</p>
-            </div>
-            <div className="flex-grow"></div>
-            <p>Notifications</p>
+            <Titlebar />
             <button className="md:hidden" onClick={props.onMenuButtonClick}>
                 <Bars3Icon className="h-6 w-6" />
             </button>
---
+++ SBR_SERV/nextjs/src/components/NavLayout/Titlebar.jsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { useStoreWeb } from "@/store/store";
+import { BellIcon } from "@heroicons/react/24/outline";
+import { Chip } from "@nextui-org/react";
+
+
+export default function Titlebar() {
+
+    const hostName = useStoreWeb((state) => state.hostName);
+    const currentPage = useStoreWeb((state) => state.currentPage);
+
+    return (
+        <div className="flex flex-grow">
+            <div className="flex-initial font-bold text-xs">
+                <Chip size="sm" color="success">{hostName}</Chip>
+            </div>
+            <div className="flex-auto text-center md:mr-[200px] font-bold md:text-lg text-md text-[#e9eee9]">
+                <p>{currentPage}</p>
+            </div>
+            <div className="flex-initial text-right">
+                <BellIcon className="w-9 pr-4"/>
+            </div>
+        </div>
+    );
+};
