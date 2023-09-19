109819667c84ac45dc3b3bcedab01b91736fe53b
Luis Arellano <luis.arellano09@gmail.com>
2023-09-19 23:58:40 +0200

    add env

--- SBR_SERV/robot_control/graphqlgen.sh
+++ SBR_SERV/robot_control/graphqlgen.sh
@@ -1,3 +1,3 @@
 # Install CLI tool:
 # cargo install graphql_client_cli 
-graphql-client introspect-schema --output ./src/graphql/schema.json  http://sbrpi.local:4000/graphql
+graphql-client introspect-schema --no-ssl --output ./src/graphql/schema.json  https://sbrpi.local/graphql
