df0f621ac780d17df1ba54dc6ba40c22a6ffd241
Luis Arellano <luis.arellano09@gmail.com>
2023-09-20 00:11:23 +0200

    add env

--- SBR_SERV/serial_connector/src/rabbitmq_producer.rs
+++ SBR_SERV/serial_connector/src/rabbitmq_producer.rs
@@ -4,6 +4,11 @@ use amiquip::{Connection, ExchangeDeclareOptions, ExchangeType, Publish};
 use serde_json::json;
 use std::{sync::mpsc::Receiver, error::Error};
 use crate::message_esp32::MessageEsp32;
+use std::env;
+
+
+//=====================================================================================================
+const URL: &str = "amqp://RABBITMQ_USER:RABBITMQ_PASS@RABBITMQ_HOST:5672/";
 
 
 //=====================================================================================================
@@ -26,8 +31,17 @@ impl RabbitmqProducer {
     
     //=====================================================================================================
     pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        
+        let rabbitmq_user = env::var("RABBITMQ_USER")?;
+        let rabbitmq_password = env::var("RABBITMQ_PASS")?;
+        let rabbitmq_host = env::var("RABBITMQ_HOST")?;
+
+        let url = URL.replace("RABBITMQ_HOST", &rabbitmq_host);
+        let url = url.replace("RABBITMQ_USER", &rabbitmq_user);
+        let url = url.replace("RABBITMQ_PASS", &rabbitmq_password);
+
         // Open connection.
-        let mut connection = Connection::insecure_open("amqp://rabbitmq:La123456.@sbr_rabbitmq:5672/")?;
+        let mut connection = Connection::insecure_open(url.as_str())?;
 
         // Open a channel - None says let the library choose the channel ID.
         let channel = connection.open_channel(None)?;
