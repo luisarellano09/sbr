f014da96aef468606c407fe55558d1375889286f
Luis Arellano <luis.arellano09@gmail.com>
2023-12-09 08:57:16 +0100

    Refactor GraphQL URL handling

--- SBR_SERV/robot_control/src/graphql.rs
+++ SBR_SERV/robot_control/src/graphql.rs
@@ -9,6 +9,17 @@ const URL: &str = "http://GRAPHQL_HOST:4000/graphql";
 
 
 //=====================================================================================================
+fn get_graphql_url() -> Result<String, Box<dyn Error>> {
+    let graphql_url = URL.replace("GRAPHQL_HOST", env::var("GRAPHQL_HOST")?.as_str());
+    Ok(graphql_url)    
+}
+
+
+//================================================================================================================================================================================================================
+// Queries
+//================================================================================================================================================================================================================
+
+//=====================================================================================================
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "src/graphql/schema.json",
@@ -18,11 +29,7 @@ const URL: &str = "http://GRAPHQL_HOST:4000/graphql";
 pub struct GetEsp32Status;
 
 pub fn query_get_esp32_status(graphql_client: &Client) -> Result<self::get_esp32_status::GetEsp32StatusGetEsp32Status, Box<dyn Error>>{
-    let response_body = post_graphql_blocking::<GetEsp32Status, _>(
-        graphql_client, 
-        URL.replace("GRAPHQL_HOST", env::var("GRAPHQL_HOST")?.as_str()), 
-        self::get_esp32_status::Variables 
-    )?;
+    let response_body = post_graphql_blocking::<GetEsp32Status, _>(graphql_client, get_graphql_url()?, self::get_esp32_status::Variables)?;
 
     if let Some(errors) = response_body.errors {
         if let Some(first_error) = errors.first() {
@@ -44,11 +51,7 @@ pub fn query_get_esp32_status(graphql_client: &Client) -> Result<self::get_esp32
 pub struct GetEsp32ModeNode1SyncData;
 
 pub fn query_get_esp32_mode_node1_sync_data(graphql_client: &Client) -> Result<self::get_esp32_mode_node1_sync_data::RegisterCommand, Box<dyn Error>>{
-    let response_body = post_graphql_blocking::<GetEsp32ModeNode1SyncData, _>(
-        graphql_client, 
-        URL.replace("GRAPHQL_HOST", env::var("GRAPHQL_HOST")?.as_str()),
-        self::get_esp32_mode_node1_sync_data::Variables 
-    )?;
+    let response_body = post_graphql_blocking::<GetEsp32ModeNode1SyncData, _>(graphql_client, get_graphql_url()?, self::get_esp32_mode_node1_sync_data::Variables)?;
 
     if let Some(errors) = response_body.errors {
         if let Some(first_error) = errors.first() {
@@ -70,11 +73,7 @@ pub fn query_get_esp32_mode_node1_sync_data(graphql_client: &Client) -> Result<s
 pub struct GetEsp32LiveIMU;
 
 pub fn query_get_esp32_live_imu(graphql_client: &Client) -> Result<self::get_esp32_live_imu::GetEsp32LiveImuGetEsp32LiveImu, Box<dyn Error>>{
-    let response_body = post_graphql_blocking::<GetEsp32LiveIMU, _>(
-        graphql_client, 
-        URL.replace("GRAPHQL_HOST", env::var("GRAPHQL_HOST")?.as_str()),
-        self::get_esp32_live_imu::Variables 
-    )?;
+    let response_body = post_graphql_blocking::<GetEsp32LiveIMU, _>(graphql_client, get_graphql_url()?, self::get_esp32_live_imu::Variables)?;
 
     if let Some(errors) = response_body.errors {
         if let Some(first_error) = errors.first() {
@@ -85,9 +84,10 @@ pub fn query_get_esp32_live_imu(graphql_client: &Client) -> Result<self::get_esp
     Ok(response_body.data.expect("Error in feching data from GraphQL").get_esp32_live_imu)
 }
 
-//==========================================================================================================================================
-//==========================================================================================================================================
 
+//================================================================================================================================================================================================================
+// Mutations
+//================================================================================================================================================================================================================
 
 //=====================================================================================================
 #[derive(GraphQLQuery)]
@@ -99,11 +99,7 @@ pub fn query_get_esp32_live_imu(graphql_client: &Client) -> Result<self::get_esp
 pub struct LoadEsp32Setup;
 
 pub fn mutation_load_esp32_setup(graphql_client: &Client) -> Result<bool, Box<dyn Error>>{
-    let response_body = post_graphql_blocking::<LoadEsp32Setup, _>(
-        graphql_client, 
-        URL.replace("GRAPHQL_HOST", env::var("GRAPHQL_HOST")?.as_str()),
-        self::load_esp32_setup::Variables 
-    )?;
+    let response_body = post_graphql_blocking::<LoadEsp32Setup, _>(graphql_client, get_graphql_url()?, self::load_esp32_setup::Variables)?;
 
     if let Some(errors) = response_body.errors {
         if let Some(first_error) = errors.first() {
@@ -125,11 +121,7 @@ pub fn mutation_load_esp32_setup(graphql_client: &Client) -> Result<bool, Box<dy
 pub struct SetEsp32ModeNode1Start;
 
 pub fn mutation_set_esp32_mode_node1_start(graphql_client: &Client) -> Result<bool, Box<dyn Error>>{
-    let response_body = post_graphql_blocking::<SetEsp32ModeNode1Start, _>(
-        graphql_client, 
-        URL.replace("GRAPHQL_HOST", env::var("GRAPHQL_HOST")?.as_str()),
-        self::set_esp32_mode_node1_start::Variables 
-    )?;
+    let response_body = post_graphql_blocking::<SetEsp32ModeNode1Start, _>(graphql_client, get_graphql_url()?, self::set_esp32_mode_node1_start::Variables)?;
 
     if let Some(errors) = response_body.errors {
         if let Some(first_error) = errors.first() {
@@ -151,11 +143,7 @@ pub fn mutation_set_esp32_mode_node1_start(graphql_client: &Client) -> Result<bo
 pub struct SetEsp32ModeNode1Stop;
 
 pub fn mutation_set_esp32_mode_node1_stop(graphql_client: &Client) -> Result<bool, Box<dyn Error>>{
-    let response_body = post_graphql_blocking::<SetEsp32ModeNode1Stop, _>(
-        graphql_client, 
-        URL.replace("GRAPHQL_HOST", env::var("GRAPHQL_HOST")?.as_str()),
-        self::set_esp32_mode_node1_stop::Variables 
-    )?;
+    let response_body = post_graphql_blocking::<SetEsp32ModeNode1Stop, _>(graphql_client, get_graphql_url()?, self::set_esp32_mode_node1_stop::Variables)?;
 
     if let Some(errors) = response_body.errors {
         if let Some(first_error) = errors.first() {
@@ -177,13 +165,7 @@ pub fn mutation_set_esp32_mode_node1_stop(graphql_client: &Client) -> Result<boo
 pub struct SetEsp32ModeNode1SyncData;
 
 pub fn mutation_set_esp32_mode_node1_sync_data(graphql_client: &Client, cmd: self::set_esp32_mode_node1_sync_data::RegisterCommand) -> Result<bool, Box<dyn Error>>{
-    let response_body = post_graphql_blocking::<SetEsp32ModeNode1SyncData, _>(
-        graphql_client, 
-        URL.replace("GRAPHQL_HOST", env::var("GRAPHQL_HOST")?.as_str()),
-        self::set_esp32_mode_node1_sync_data::Variables {
-            sync_status: cmd
-        }
-    )?;
+    let response_body = post_graphql_blocking::<SetEsp32ModeNode1SyncData, _>(graphql_client, get_graphql_url()?, self::set_esp32_mode_node1_sync_data::Variables {sync_status: cmd})?;
 
     if let Some(errors) = response_body.errors {
         if let Some(first_error) = errors.first() {
