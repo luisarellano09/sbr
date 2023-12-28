c3ec265662ae521415b75dcd0334f62b6e3e78e2
Luis Arellano <luis.arellano09@gmail.com>
2023-12-28 20:04:13 +0100

    Add video source and output in main.py

--- SBR_SERV/camera_streamer/src/main.py
+++ SBR_SERV/camera_streamer/src/main.py
@@ -1,8 +1,30 @@
 import sys
-import time
 from jetson_utils import videoSource, videoOutput, Log
 
+# create video sources & outputs
+input = videoSource("/dev/video1", argv=["--input-width=1280", "--input-height=720", "--input-rate=30", "--input-codec=H264"])
+output = videoOutput("rtsp://@:6000/d435/rgb", argv=[""])
+
+# capture frames until EOS or user exits
+numFrames = 0
+
 while True:
-    time.sleep(2)
-    print("Hello World 4")
+    # capture the next image
+    img = input.Capture()
+
+    if img is None: # timeout
+        continue  
+
+    numFrames += 1
+
+    # render the image
+    output.Render(img)
     
+    # update the title bar
+    if numFrames % 50 == 0:
+        Log.Verbose(f"video-viewer:  captured {numFrames} frames ({img.width} x {img.height})")
+        print(f" {img.width}x{img.height} | {output.GetFrameRate()} FPS")
+
+    # exit on input/output EOS
+    if not input.IsStreaming() or not output.IsStreaming():
+        break
