dac8b89fbe729b610c41d6543537ae48475ff2aa
Luis Arellano <luis.arellano09@gmail.com>
2023-12-29 02:43:31 +0100

    Commented out color frame processing code

--- SBR_SERV/camera_streamer/src/main.py
+++ SBR_SERV/camera_streamer/src/main.py
@@ -43,7 +43,7 @@ def task_camera_depth_process(streamerDepth):
     pipe = rs.pipeline()
     cfg  = rs.config()
 
-    cfg.enable_stream(rs.stream.color, 1280,720, rs.format.rgb8, 30)
+    # cfg.enable_stream(rs.stream.color, 1280,720, rs.format.rgb8, 30)
     cfg.enable_stream(rs.stream.depth, 1280,720, rs.format.z16, 30)
 
     pipe.start(cfg)
@@ -54,17 +54,17 @@ def task_camera_depth_process(streamerDepth):
     while True:
         frame = pipe.wait_for_frames()
         depth_frame = frame.get_depth_frame()
-        color_frame = frame.get_color_frame()
+        # color_frame = frame.get_color_frame()
 
-        # depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
+        depth_image = np.asanyarray(depth_frame.get_data())
+        # color_image = np.asanyarray(color_frame.get_data())
         
-        gsFrameRGB = cudaFromNumpy(color_image)
-        # gsFrameDepth = cudaFromNumpy(depth_image)
+        # gsFrameRGB = cudaFromNumpy(color_image)
+        gsFrameDepth = cudaFromNumpy(depth_image)
 
         # Render the image
-        streamerDepth.Render(gsFrameRGB)
-        # streamerDepth.Render(gsFrameDepth)
+        # streamerDepth.Render(gsFrameRGB)
+        streamerDepth.Render(gsFrameDepth)
 
 
 if __name__ == '__main__':
