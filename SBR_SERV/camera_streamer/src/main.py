a46f0fb1d24237aa562433bd577416a60c29544a
Luis Arellano <luis.arellano09@gmail.com>
2023-12-29 04:43:06 +0100

    Add depth scaling and background removal to camera streamer

--- SBR_SERV/camera_streamer/src/main.py
+++ SBR_SERV/camera_streamer/src/main.py
@@ -16,7 +16,16 @@ cfg.enable_stream(rs.stream.color, 848,480, rs.format.rgb8, 60)
 cfg.enable_stream(rs.stream.depth, 848,480, rs.format.z16, 60)
 
 # Start streaming
-pipe.start(cfg)
+profile = pipe.start(cfg)
+
+# Getting the depth sensor's depth scale (see rs-align example for explanation)
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+print("Depth Scale is: " , depth_scale)
+
+#  clipping_distance_in_meters meters away
+clipping_distance_in_meters = 1 #1 meter
+clipping_distance = clipping_distance_in_meters / depth_scale
 
 while True:
     frame = pipe.wait_for_frames()
@@ -26,7 +35,17 @@ while True:
     depth_image = np.asanyarray(depth_frame.get_data())
     color_image = np.asanyarray(color_frame.get_data())
 
-    adjustedDepth = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.5), cv2.COLORMAP_JET)
+     # Remove background - Set pixels further than clipping_distance to grey
+    grey_color = 153
+    depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+    bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+
+    # Render images:
+    #   depth align to color on left
+    #   depth on right
+    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+    adjustedDepth = np.hstack((bg_removed, depth_colormap))
+    
     adjustedRGB = cv2.addWeighted( color_image, 1, color_image, 0, 15)
 
     gsFrameDepth = cudaFromNumpy(adjustedDepth)
@@ -35,3 +54,8 @@ while True:
     # Render the image
     streamerCameraDepth.Render(gsFrameDepth)
     streamerCameraRGB.Render(gsFrameRGB)
+
+
+# Info: To receive the stream use the following command
+# gst-launch-1.0 -v rtspsrc location=rtsp://sbrnx:6000/d435/depth latency=0 ! rtph264depay ! h264parse ! avdec_h264 ! autovideosink
+# gst-launch-1.0 -v rtspsrc location=rtsp://sbrnx:6000/d435/rgb latency=0 ! rtph264depay ! h264parse ! avdec_h264 ! autovideosink
