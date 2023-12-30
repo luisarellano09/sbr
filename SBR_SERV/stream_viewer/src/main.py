2d0c3eba274ad691cc14b90688149f98f11488cc
Luis Arellano <luis.arellano09@gmail.com>
2023-12-30 13:59:09 +0100

    Add RTSP stream and display frames in a window

--- SBR_SERV/stream_viewer/src/main.py
+++ SBR_SERV/stream_viewer/src/main.py
@@ -3,14 +3,25 @@ import cv2
 
 
 def task_camera_process(stream_path):
+    # Create rstp stream
     stream = cv2.VideoCapture(stream_path, cv2.CAP_GSTREAMER)
+    
+    # Check if stream is opened
     if not stream.isOpened():
         print("Error opening stream: " + stream_path)
         return
+    
+    # Create window
     cv2.namedWindow(stream_path)
+    
     while True:
+        # Read frame
         _, frame = stream.read()
+        
+        # Show frame
         cv2.imshow(stream_path, frame)
+        
+        # Wait for key
         key = cv2.waitKey(1)
         if key == 27:  # Press ESC to exit/close each window.
             break
