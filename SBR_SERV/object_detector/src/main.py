06bb7db3e1397c116b401debb7a763ec6d4e866d
Luis Arellano <luis.arellano09@gmail.com>
2023-12-31 01:13:03 +0100

    Refactor video output in main.py

--- SBR_SERV/object_detector/src/main.py
+++ SBR_SERV/object_detector/src/main.py
@@ -22,23 +22,16 @@ if not cam.isOpened():
 
 # Create rstp
 #streamerObject = videoOutput("rtsp://@:6001/object_detector/t1")
-out = cv2.VideoWriter('appsrc ! videoconvert ! video/x-raw,format=I420 ! x264enc tune=zerolatency bitrate=600 speed-preset=superfast key-int-max=60 ! video/x-h264,profile=baseline  ! rtspclientsink location=rtsp://sbrnx:6001/object_detector/t1', cv2.CAP_GSTREAMER, 0, 30, (1280, 720), True)
+out = cv2.VideoWriter('location=rtsp://sbrnx:6001/object_detector/t1', cv2.CAP_GSTREAMER, 0, 30, (1280, 720))
 if not out.isOpened():
     raise Exception("can't open video writer")
 
 while True:
-    # _, frame = cam.read()
-    # frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # print(frame_gray.shape)
+    _, frame = cam.read()
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    print(frame_gray.shape)
     
     frame = np.zeros((height, width, 3), np.uint8)
 
-    # create a rectangle
-    color = colors[curcolor]
-    curcolor += 1
-    curcolor %= len(colors)
-    for y in range(0, int(frame.shape[0] / 2)):
-        for x in range(0, int(frame.shape[1] / 2)):
-            frame[y][x] = color
     out.write(frame)
     
