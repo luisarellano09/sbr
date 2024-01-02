8a7206e61ab3648da1c4982af76c5ccca1ebc444
Luis Arellano <luis.arellano09@gmail.com>
2024-01-02 07:39:33 +0100

    Update frame color conversion in main.py

--- SBR_SERV/object_detector/src/main.py
+++ SBR_SERV/object_detector/src/main.py
@@ -18,5 +18,5 @@ while True:
     _, frame = cam.read()
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     print(frame_gray.shape)
-    streamerObjectDetector.Render(cudaFromNumpy(frame))
+    streamerObjectDetector.Render(cudaFromNumpy(frame_gray))
     
