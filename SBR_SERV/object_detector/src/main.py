47b05b81369f5e82b5bca164e6aac7b1e8791345
Luis Arellano <luis.arellano09@gmail.com>
2024-01-04 23:19:17 +0100

    Update Dockerfile and main.py

--- SBR_SERV/object_detector/src/main.py
+++ SBR_SERV/object_detector/src/main.py
@@ -1,23 +1,20 @@
-from jetson_utils import videoOutput, cudaFromNumpy
 import numpy as np
-#import cv2
+import cv2
 
 
 # Create Viceo Capture object with GStreamer pipeline
 stream_path_D435_RGB = "rtsp://sbrnx:6000/d435/rgb"
-#cam = cv2.VideoCapture(stream_path_D435_RGB, cv2.CAP_GSTREAMER)
-# if not cam.isOpened():
-#     print("Error opening stream: " + stream_path_D435_RGB)
-#     exit(1)
+cam = cv2.VideoCapture(stream_path_D435_RGB, cv2.CAP_GSTREAMER)
+if not cam.isOpened():
+    print("Error opening stream: " + stream_path_D435_RGB)
+    exit(1)
 
 # Create rstp stream
 stream_path_ObjectDetector = "rtsp://@:6000/serv/object_detector"
-streamerObjectDetector = videoOutput(stream_path_ObjectDetector)
+
 
 while True:
-    # _, frame = cam.read()
-    # frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # print(frame_gray.shape)
-    frame_gray = np.random.randint(0, 255, (720, 1280), dtype=np.uint8)
-    streamerObjectDetector.Render(cudaFromNumpy(frame_gray))
+    _, frame = cam.read()
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    print(frame_gray.shape)
     
