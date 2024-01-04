3fca0a60dc8e9aeeac16f9598207aeb3523b67d8
Luis Arellano <luis.arellano09@gmail.com>
2024-01-04 23:35:43 +0100

    Add video recording functionality

--- SBR_SERV/object_detector/src/main.py
+++ SBR_SERV/object_detector/src/main.py
@@ -13,8 +13,23 @@ if not cam.isOpened():
 stream_path_ObjectDetector = "rtsp://@:6000/serv/object_detector"
 
 
+fps = 30
+width = 1280
+height = 720
+
+out = cv2.VideoWriter('appsrc ! videoconvert' + \
+    ' ! video/x-raw,format=I420' + \
+    ' ! x264enc speed-preset=ultrafast bitrate=600 key-int-max=' + str(fps * 2) + \
+    ' ! video/x-h264,profile=baseline' + \
+    ' ! rtspclientsink location=rtsp://localhost:6000/serv/object_detector',
+    cv2.CAP_GSTREAMER, 0, fps, (width, height), True)
+if not out.isOpened():
+    raise Exception("can't open video writer")
+
+
 while True:
     _, frame = cam.read()
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     print(frame_gray.shape)
+    out.write(frame)
     
