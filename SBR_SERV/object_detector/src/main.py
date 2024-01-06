d1c8331ad47f31da3157798a0ffa01f39c42da2c
Luis Arellano <luis.arellano09@gmail.com>
2024-01-06 19:43:22 +0100

    Update Dockerfile and main.py for object detector

--- SBR_SERV/object_detector/src/main.py
+++ SBR_SERV/object_detector/src/main.py
@@ -1,21 +1,19 @@
-from jetson_utils import videoOutput, cudaFromNumpy
+from jetson_utils import videoSource, videoOutput, cudaFromNumpy, cudaAllocMapped, cudaConvertColor, cudaToNumpy, cudaDeviceSynchronize
 import numpy as np
 import cv2
 
 
-# Create Viceo Capture object with GStreamer pipeline
-stream_path_D435_RGB = "rtsp://sbrnx:6000/d435/rgb "
-cam = cv2.VideoCapture(stream_path_D435_RGB, cv2.CAP_GSTREAMER)
-if not cam.isOpened():
-    print("Error opening stream: " + stream_path_D435_RGB)
-    exit(1)
+# Create Video Source
+cameraRGB = videoSource("rtsp://sbrnx:6000/d435/rgb")
 
-# Create rstp stream
-stream_path_ObjectDetector = "rtsp://@:6000/serv/object_detector --output-codec=h265 --output-encoder=v4l2"
+# Create Video Output
 streamerObjectDetector = videoOutput("rtsp://@:6001/serv/object_detector")
 
 while True:
-    _, frame = cam.read()
-    frame_cv = cv2.putText(frame, "Hello World", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 10, (0, 0, 255), 2)
-    streamerObjectDetector.Render(cudaFromNumpy(frame_cv))
-    
+    image = cameraRGB.Capture()
+    bgr_img = cudaAllocMapped(width=image.width, height=image.height, format='bgr8')
+    cudaConvertColor(image, bgr_img)
+    cv_img = cudaToNumpy(bgr_img)
+    cudaDeviceSynchronize()
+    frame = cv2.putText(cv_img, "Hello World", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 10, (0, 0, 255), 2)
+    streamerObjectDetector.Render(cudaFromNumpy(frame))
