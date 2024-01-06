6b3134f16e9d44e3d87e03211e390c473356e3a2
Luis Arellano <luis.arellano09@gmail.com>
2024-01-06 18:16:26 +0100

    Add Dockerfile for object detector and modify main.py

--- SBR_SERV/object_detector/src/main.py
+++ SBR_SERV/object_detector/src/main.py
@@ -12,6 +12,6 @@ streamerObjectDetector = videoOutput("rtsp://@:6001/serv/object_detector")
 while True:
     image = cameraRGB.Capture()
     frame = np.asanyarray(image)
-    frame_flip = cv2.flip(frame, 1)
+    cv2.putText(frame, "Hello World", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
     streamerObjectDetector.Render(cudaFromNumpy(frame))
     
