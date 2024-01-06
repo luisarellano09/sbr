49f9d11e01aebca2b0cdda5a777ea95e2c5cbac9
Luis Arellano <luis.arellano09@gmail.com>
2024-01-06 17:51:40 +0100

    Add videoSource for RGB camera and update main loop to capture frames from videoSource

--- SBR_SERV/object_detector/src/main.py
+++ SBR_SERV/object_detector/src/main.py
@@ -1,21 +1,17 @@
-from jetson_utils import videoOutput, cudaFromNumpy
+from jetson_utils import videoSource, videoOutput, cudaFromNumpy
 import numpy as np
 import cv2
 
 
-# Create Viceo Capture object with GStreamer pipeline
-stream_path_D435_RGB = "rtsp://sbrnx:6000/d435/rgb"
-cam = cv2.VideoCapture(stream_path_D435_RGB, cv2.CAP_GSTREAMER)
-if not cam.isOpened():
-    print("Error opening stream: " + stream_path_D435_RGB)
-    exit(1)
+# Create Video Source
+cameraRGB = videoSource("rtsp://sbrnx:6000/d435/rgb")
 
-# Create rstp stream
-stream_path_ObjectDetector = "rtsp://@:6000/serv/object_detector --output-codec=h265 --output-encoder=v4l2"
+# Create Video Output
 streamerObjectDetector = videoOutput("rtsp://@:6001/serv/object_detector")
 
 while True:
-    _, frame = cam.read()
+    image = cameraRGB.Capture()
+    frame = np.asanyarray(image)
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     print(frame_gray.shape)
     streamerObjectDetector.Render(cudaFromNumpy(frame))
