2faaa602535cdab32e2e266db55f62169414c6e2
Luis Arellano <luis.arellano09@gmail.com>
2024-01-06 20:02:16 +0100

    Add text overlay to object detector output

--- SBR_SERV/object_detector/src/main.py
+++ SBR_SERV/object_detector/src/main.py
@@ -1,17 +1,31 @@
 from jetson_utils import videoSource, videoOutput, cudaFromNumpy, cudaAllocMapped, cudaConvertColor, cudaToNumpy, cudaDeviceSynchronize
 import cv2
 
+# Main
+if __name__ == '__main__':
 
-# Create Video Source
-cameraRGB = videoSource("rtsp://sbrnx:6000/d435/rgb")
+    # Create Video Source
+    cameraRGB = videoSource("rtsp://sbrnx:6000/d435/rgb")
 
-# Create Video Output
-streamerObjectDetector = videoOutput("rtsp://@:6001/serv/object_detector")
+    # Create Video Output
+    streamerObjectDetector = videoOutput("rtsp://@:6001/serv/object_detector")
 
-while True:
-    # Read frame from RRSP stream
-    cuda_image = cameraRGB.Capture()
+    while True:
+        # Read frame from RRSP stream
+        cuda_image = cameraRGB.Capture()
 
+        # Convert image to numpy (opencv format)
+        cv_image = cuda_to_cv2(cuda_image)
+
+        # Add text
+        cv_image_text = cv2.putText(cv_image, "Hello World", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        
+        # Render the image
+        streamerObjectDetector.Render(cudaFromNumpy(cv_image_text))
+
+
+# Function to convert cuda image to cv2 image
+def cuda_to_cv2(cuda_image):
     # Allocate new image
     rgb_image = cudaAllocMapped(width=cuda_image.width, height=cuda_image.height, format='bgr8')
 
@@ -27,8 +41,4 @@ while True:
     # Convert image to BGR
     cv_image = cv2.cvtColor(cv_rgb_image, cv2.COLOR_RGB2BGR)
 
-    # Add text
-    cv_image_text = cv2.putText(cv_image, "Hello World", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    
-    # Render the image
-    streamerObjectDetector.Render(cudaFromNumpy(cv_image_text))
+    return cv_image
