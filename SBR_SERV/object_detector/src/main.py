1515f1a3c90efc17d9364442b10574018ee19262
Luis Arellano <luis.arellano09@gmail.com>
2024-01-06 09:10:59 +0100

    Update object detector Dockerfile and main.py

--- SBR_SERV/object_detector/src/main.py
+++ SBR_SERV/object_detector/src/main.py
@@ -1,4 +1,4 @@
-# from jetson_utils import videoOutput, cudaFromNumpy
+from jetson_utils import videoOutput, cudaFromNumpy
 import numpy as np
 import cv2
 
@@ -12,11 +12,11 @@ if not cam.isOpened():
 
 # Create rstp stream
 stream_path_ObjectDetector = "rtsp://@:6000/serv/object_detector --output-codec=h265 --output-encoder=v4l2"
-# streamerObjectDetector = videoOutput("rtsp://@:6000/serv/object_detector --output-codec=h265 --output-encoder=v4l2")
+streamerObjectDetector = videoOutput("rtsp://@:6001/serv/object_detector")
 
 while True:
     _, frame = cam.read()
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     print(frame_gray.shape)
-    #streamerObjectDetector.Render(cudaFromNumpy(frame))
+    streamerObjectDetector.Render(cudaFromNumpy(frame))
     
