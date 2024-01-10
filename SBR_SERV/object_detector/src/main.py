bc68f7a2227e6952138ac6165f9aba10850ebb99
Luis Arellano <luis.arellano09@gmail.com>
2024-01-10 06:29:46 +0100

    Remove text overlay in object detection rendering

--- SBR_SERV/object_detector/src/main.py
+++ SBR_SERV/object_detector/src/main.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
         cv_image = cuda_to_cv2(cuda_image)
 
         # Add text
-        cv_image_text = cv2.putText(cv_image, net.GetNetworkFPS(),  (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        # cv2.putText(cv_image, net.GetNetworkFPS(),  (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
         
         # Render the image
-        streamerObjectDetector.Render(cv2_to_cuda(cv_image_text))
+        streamerObjectDetector.Render(cv2_to_cuda(cv_image))
