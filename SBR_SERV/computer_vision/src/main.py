e5c32fd99d9049198d2021a56f5ea26f7ccea9ca
Luis Arellano <luis.arellano09@gmail.com>
2024-01-15 00:14:55 +0100

    Add depth image streaming functionality

--- SBR_SERV/computer_vision/src/main.py
+++ SBR_SERV/computer_vision/src/main.py
@@ -12,7 +12,7 @@ def cv2_to_cuda(cv_image):
     return cudaFromNumpy(cv_image)
 
 
-def task_read_camera(queue_streamer_camera):
+def task_read_camera(queue_streamer_camera, queue_streamer_camera_depth):
      # Create pipeline
     pipe = rs.pipeline()
     cfg  = rs.config()
@@ -52,11 +52,26 @@ def task_read_camera(queue_streamer_camera):
         # Normalice depth image
         depth_image = cv2.multiply(depth_image, distance_factor)
 
+         # Divide the distance (one channel) into 3 channels. For example 600 => (90,255,255)
+        depth_image_1 = np.where(depth_image > 255, 255, depth_image)
+
+        depth_image_temp = np.add(depth_image, -255)
+        depth_image_temp = np.where(depth_image_temp < 0, 0, depth_image_temp)
+        depth_image_2 = np.where(depth_image_temp > 255, 255, depth_image_temp)
+
+        depth_image_temp = np.add(depth_image, -510)
+        depth_image_temp = np.where(depth_image_temp < 0, 0, depth_image_temp)
+        depth_image_3 = np.where(depth_image_temp > 255, 255, depth_image_temp)
+
+        # Merge the 3 channels into one image
+        depth_image = np.dstack((depth_image_3, depth_image_2, depth_image_1)) 
+
         # Adjust image RGB
         # color_image = cv2.addWeighted( color_image, 1, color_image, 0, 15)
 
         # Render the image
         queue_streamer_camera.put(color_image)
+        queue_streamer_camera_depth.put(depth_image)
 
         # get the end time
         et = time.time()
@@ -79,22 +94,39 @@ def task_streamer_camera(queue_streamer_camera):
         streamerCameraRGB.Render(cv2_to_cuda(color_image))
 
 
+def task_streamer_camera_depth(queue_streamer_camera_depth):
+    # Create rstp stream
+    streamerCameraDepth = videoOutput("rtsp://@:6001/d435/depth")
+
+    while True:
+
+        # Get image from queue
+        depth_image = queue_streamer_camera_depth.get()
+
+        # Render the image
+        streamerCameraDepth.Render(cv2_to_cuda(depth_image))
+
+
 # Main
 if __name__ == '__main__':
 
     # Create queue
     queue_streamer_camera = Queue()
+    queue_streamer_camera_depth = Queue()
 
     # Create threads
-    thread_read_camera = threading.Thread(target=task_read_camera, args=(queue_streamer_camera,))
+    thread_read_camera = threading.Thread(target=task_read_camera, args=(queue_streamer_camera, queue_streamer_camera_depth))
     thread_streamer_camera = threading.Thread(target=task_streamer_camera, args=(queue_streamer_camera,))
+    thread_streamer_camera_depth = threading.Thread(target=task_streamer_camera_depth, args=(queue_streamer_camera_depth,))
 
     # Start threads
     thread_read_camera.start()
     thread_streamer_camera.start()
+    thread_streamer_camera_depth.start()
 
     # Wait threads
     thread_read_camera.join()
     thread_streamer_camera.join()
+    thread_streamer_camera_depth.join()
 
 
