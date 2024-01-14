30e43dd249dd550abd898fc4753bab7991317eaf
Luis Arellano <luis.arellano09@gmail.com>
2024-01-14 11:48:10 +0100

    Update videoOutput parameters in main.py

--- SBR_SERV/camera_streamer/src/main.py
+++ SBR_SERV/camera_streamer/src/main.py
@@ -13,7 +13,7 @@ def cv2_to_cuda(cv_image):
 if __name__ == '__main__':
     # Create rstp streams
     streamerCameraDepth = videoOutput("rtsp://@:6000/d435/depth")
-    streamerCameraRGB = videoOutput("rtsp://@:6000/d435/rgb --headless --output-codec=h265")
+    streamerCameraRGB = videoOutput("rtsp://@:6000/d435/rgb --output-encoder=v4l2 -- bitrate=10000000")
 
     # Create pipeline
     pipe = rs.pipeline()
