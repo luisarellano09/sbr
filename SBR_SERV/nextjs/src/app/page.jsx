73d4a94aa424b8e2b1b519633584ec3700a1393d
Luis Arellano <luis.arellano09@gmail.com>
2024-01-21 23:29:48 +0100

    Refactor page layout to include dashboard and control components

--- SBR_SERV/nextjs/src/app/page.jsx
+++ SBR_SERV/nextjs/src/app/page.jsx
@@ -14,11 +14,12 @@ export default function Home() {
     },[]);
 
 	return (
-        <div className="h-[calc(100vh-74px)]">
-            <DashboardRobotStatus />
-            <ControlRobotCcommand />
-            
-
+        <div className="h-[calc(100vh-74px)] md:flex justify-start">
+            <div className="pl-5 pt-5 pb-5">
+                <DashboardRobotStatus />
+                <ControlRobotCcommand />
+            </div>
+            <iframe src={"http://sbrnx.local:8554"} className="p-5 w-full h-[70%] max-w-[1350px]"/>
         </div>
     );
 }
